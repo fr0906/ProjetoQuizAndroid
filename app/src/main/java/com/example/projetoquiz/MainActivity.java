f8a77252b04e29272393d72db67667608c22b71b
fr0906 <franciellysouzax@gmail.com>
2025-09-19 22:53:17 -0300

    fran

--- app/src/main/java/com/example/projetoquiz/MainActivity.java
+++ app/src/main/java/com/example/projetoquiz/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.projetoquiz;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,55 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView txvPergunta;
+    RadioGroup rdgPrincipal;
+    Button btnResponder;
+    int prontuacao = 0;
+    int indicePergunta = 0;
+
+    String[] perguntas = {
+            "Minha pergunta 1",
+            "Minha pergunta 2",
+            "Minha pergunta 3",
+            "Minha pergunta 4",
+            "Minha pergunta 5"
+    };
+
+    int[] respoastasCorretas = {
+            R.id.rbA,
+            R.id.rbB,
+            R.id.rbC,
+            R.id.rbD
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        txvPergunta = findViewById(R.id.txvPergunta);
+        rdgPrincipal = findViewById(R.id.rdgPrincipal);
+        btnResponder = findViewById(R.id.btnResponder);
+
+        carregarPerguntas();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void carregarPerguntas() {
+        if (indicePergunta < perguntas.length) {
+            txvPergunta.setText(perguntas[indicePergunta]);
+            rdgPrincipal.clearCheck();
+            btnResponder.setEnabled(true);
+        } else {
+            txvPergunta.setText("Fim");
+            btnResponder.setEnabled(false);
+        }
+
     }
 }
